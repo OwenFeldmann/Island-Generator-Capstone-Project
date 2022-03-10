799856408a2d4adb959b0f4ffd0651193ddcd66f
OwenFeldmann <ofeldman@unca.edu>
2022-03-10 17:29:02 -0500

    Volcano settings submenu added

--- Assets/UI/SettingsMenu.cs
+++ Assets/UI/SettingsMenu.cs
@@ -33,6 +33,19 @@ public class SettingsMenu : MonoBehaviour
 	public InputField biomeCountInputField;
 	public InputField biomeSpreadInputField;
 	
+	[Header("Volcano Settings")]
+	public Toggle randomCenterToggle;
+	public Slider volcanoXCenterSlider;
+	public Slider volcanoZCenterSlider;
+	public InputField lavaLevelInputField;
+	public InputField rimRadiusInputField;
+	public InputField iterationsInputField;
+	public InputField iterationStrengthInputField;
+	public InputField startRadiusInputField;
+	public InputField endRadiusInputField;
+	public InputField startSpreadInputField;
+	public InputField endSpreadInputField;
+	
 	public void ViewIsland()
 	{
 		settingsMenu.SetActive(false);
@@ -66,12 +79,8 @@ public class SettingsMenu : MonoBehaviour
 		mg.seed = int.Parse(seedInputField.text);
 		mg.xSize = (int) xSizeSlider.value;
 		mg.zSize = (int) zSizeSlider.value;
-		mg.xCenter = (int) xCenterSlider.value;
-		if(mg.xCenter > mg.xSize)//center can't be outside of bounds
-			mg.xCenter = mg.xSize/2;
-		mg.zCenter = (int) zCenterSlider.value;
-		if(mg.zCenter > mg.zSize)//center can't be outside of bounds
-			mg.zCenter = mg.zSize/2;
+		mg.xCenter = (int) (xCenterSlider.value * mg.xSize);//slider [0,1] is the percent of the island
+		mg.zCenter = (int) (zCenterSlider.value * mg.zSize);
 		mg.noiseXScale = xNoiseScaleSlider.value;
 		mg.noiseZScale = zNoiseScaleSlider.value;
 		mg.seaLevel = Mathf.Abs(float.Parse(seaLevelInputField.text));
@@ -82,6 +91,31 @@ public class SettingsMenu : MonoBehaviour
 		if(bg.biomesToPlace < 1)
 			bg.biomesToPlace = 1;
 		bg.maxBiomeSpread = Mathf.Abs(float.Parse(biomeSpreadInputField.text));
+		
+		//Volcano Settings
+		vg.chooseRandomCenter = randomCenterToggle.isOn;
+		vg.centerX = volcanoXCenterSlider.value * mg.xSize;//slider [0,1] is the percent of the island
+		vg.centerZ = volcanoZCenterSlider.value * mg.zSize;
+		vg.lavaLevel = float.Parse(lavaLevelInputField.text);
+		vg.rimRadius = Mathf.Abs(float.Parse(rimRadiusInputField.text));
+		vg.iterations = Mathf.Abs(int.Parse(iterationsInputField.text));
+		vg.iterationStrength = Mathf.Abs(float.Parse(iterationStrengthInputField.text));
+		vg.iterationStartRadius = Mathf.Abs(float.Parse(startRadiusInputField.text));
+		vg.iterationEndRadius = Mathf.Abs(float.Parse(endRadiusInputField.text));
+		vg.startSpread = Mathf.Abs(float.Parse(startSpreadInputField.text));
+		vg.endSpread = Mathf.Abs(float.Parse(endSpreadInputField.text));
+		
+		if(vg.iterationStartRadius < vg.iterationEndRadius){//ensure start radius is larger than the end radius
+			float temp = vg.iterationStartRadius;
+			vg.iterationStartRadius = vg.iterationEndRadius;
+			vg.iterationEndRadius = temp;
+		}
+		
+		if(vg.startSpread < vg.endSpread){//ensure start spread is larger than the end spread
+			float temp = vg.startSpread;
+			vg.startSpread = vg.endSpread;
+			vg.endSpread = temp;
+		}
 		
 		mg.StartIslandGeneration();
 	}
